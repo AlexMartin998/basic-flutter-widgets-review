ead013e850883fd40c99a4428c8ce1c66da09a8d
Adrian <alextravc.342@gmail.com>
2023-10-14 14:35:42 -0500

    [ADD]: naming router (go_router)

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -4,6 +4,9 @@ import 'package:widget_app/config/menu/menu_items.dart';
 
 
 class HomeScreen extends StatelessWidget {
+  // naming routes
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
 
@@ -61,18 +64,22 @@ class _CustomListTile extends StatelessWidget {
 
       onTap: () {
         // // // navigation: si tenemos Appbar, en auto coloca el btn de retroceder
+
         // // go_router: extiende el context de la app, x eso podemos usar el   .push
         context.push(menuItem.link);
+        // context.pushNamed(CardsScreen.name); // requeriri mas config en el menu item y demas para q sea dinamico
 
 
-        // // Navigator WITHOUT naming | push: hace 1 Stack de pantallas (1 sobre otra)
+
+        // // sistema de rutas propias de flutter:
+        // Navigator WITHOUT naming | push: hace 1 Stack de pantallas (1 sobre otra)
         /* Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) => const ButtonsScreen(),
           ),
         ); */
 
-        // // named routes (limitations)
+        // Named routes (limitations)
         // Navigator.pushNamed(context, menuItem.link);
       },
       
