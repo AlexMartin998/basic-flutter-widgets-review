c865a085b2ae55f000302f8ca15b0b33561831fc
Adrian <alextravc.342@gmail.com>
2023-10-14 11:47:28 -0500

    [ADD]: app theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const List<Color> _colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }) : assert(selectedColor <= _colorList.length - 1 && selectedColor >= 0, 'Invalid index color');
+
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: _colorList[selectedColor],
+  );
+}
